79b0e9cbca53c01ac1eb7cda35efcc7e1a4ba1ef
taluna <taluna@gmail.com>
2012-12-17 00:55:57 -0200

    Uso do Easyb

---
+++ test/unit/br/org/coletar/domain/DonatorStory.groovy
@@ -0,0 +1,18 @@
+package br.org.coletar.domain
+
+import br.org.coletar.domain.Donator
+
+scenario "Donator cannot be created without a user", {
+            given "A new Donator" , {
+
+            }
+            when "Save donator with null user",{
+
+            }
+            then "the donator validation shows error msg",{
+
+            }
+}
+
+
+
